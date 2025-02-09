efee15b6ac1223defe787221471ba47cca976cda
Bhumika Krishnamurthy <bhumikakrishnamurthy@Bhumikas-MacBook-Air-2.local>
2025-02-09 16:52:53 +0530

    Initial commit

---
+++ hms-backend/api/Dbconnect.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Access-Control-Allow-Origin: http://localhost/hms-backend/:3000');
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+	/**
+	* Database Connection
+	*/
+	class DbConnect {
+		private $server = 'localhost';
+		private $dbname = 'healthhub';
+		private $user = 'root';
+		private $pass = '';
+
+		public function connect() {
+			try {
+				$conn = new PDO('mysql:host=' .$this->server .';dbname=' . $this->dbname, $this->user, $this->pass);
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				return $conn;
+			} catch (\Exception $e) {
+				echo "Database Error: " . $e->getMessage();
+			}
+		}
+        
+	}
+?>
